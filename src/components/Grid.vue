0c0a589ca75394c91153d9a20e924148d6edd066
Mohammed Al Naji <ipadmohammed0@gmail.com>
2025-07-04 10:49:30 +0300

    to the pc

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,12 +1,60 @@
 <script setup>
-import { workoutProgram } from '../utils';
-const workoutTypes= ['Push', 'Pull', 'Legs'];
+import { workoutProgram } from "../utils";
+const workoutTypes = ["Push", "Pull", "Legs"];
 </script>
 
 <template>
-    <div>Hi</div>
+	<section class="align-items-center d-flex flex-column">
+		<div
+			class="d-flex flex-wrap justify-content-between gap-2 mx-2 container p-3"
+		>
+			<button
+				:key="Index"
+				v-for="(workout, Index) in Object.keys(workoutProgram)"
+				class="border-0 bg-transparent"
+			>
+				<div
+					class="card align-items-center cardwidth d-flex flex-column gap-2 p-1 rounded-3 hovering"
+				>
+					<img
+						src="../../public/icons8-triceps-100.png"
+						alt="triceps"
+						class=""
+						width="50"
+						height="50"
+                        v-if ="Index %3 === 0"
+					/>
+					<img
+						src="../../public/icons8-biceps-100.png"
+						alt="triceps"
+						class=""
+						width="50"
+						height="50"
+                        v-if="Index % 3 === 1"
+					/>
+					<img
+						src="../../public/icons8-leg-100.png"
+						alt="triceps"
+						class=""
+						width="50"
+						height="50"
+                        v-if="Index % 3 === 2"
+					/>
+					<div>
+						<h4 class="font1">Day {{ Index + 1 }}</h4>
+						<p class="font2">{{ workoutTypes[Index % 3] }}</p>
+					</div>
+				</div>
+			</button>
+		</div>
+	</section>
 </template>
 
 <style scoped>
+.cardwidth {
+	width: 100px;
+	height: 122px;
+	
+}
 
 </style>
--- src/components/pages/Dashboard.vue
+++ src/components/pages/Dashboard.vue
@@ -6,8 +6,19 @@ const todayFact = gymHealthFacts[randoming]
 </script>
 
 <template>
-    <section id="dashboard">
-        <div class="">{{ todayFact }}</div>
+    <section id="dashboard" class="d-flex flex-column gap-5">
+        <div class="d-flex flex-wrap justify-content-center">
+            <div class="d-flex flex-column gap-2 align-items-center">
+                <h2 class="font2">Welcome GymMan!</h2>
+                <h3 class="font1">Todays Fact is:</h3
+            </div
+            <div class="d-flex flex-column align-items-center">    
+                <h5 class="">{{ todayFact }}</h5>
+                <div class="d-flex flex-row justify-content-center">
+                    <button class="btn btn-dark fs-4 hovering fw-bold">Start Workout</button>
+                </div>
+            </div> 
+        </div>
         <Grid />
     </section>
 </template>
